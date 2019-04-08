f0c8c1b425eccc126415fc625e73f1659a30f963
DmitryVatulin <47992419+DmitryVatulin@users.noreply.github.com>
2019-04-08 22:26:55 +0300

    Create 3.1.cpp

---
+++ lab3/3.1.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Child {
+    private:
+        string name;
+        string family;
+        int age;
+    public:
+        void set_name(string name) {
+            this->name = name;
+        };
+
+        void set_family(string family) {
+            this->family = family;
+        };
+
+        void set_age(int age) {
+            this->age = age;
+        };
+
+        void print_value() {
+            cout << "===================" << endl;
+            cout << "Имя: " << name << endl;
+            cout << "Фамилия: " << family << endl;
+            cout << "Возраст: " << age << endl;
+            cout << "===================" << endl;
+        };
+};
+
+int main(int argc, char const *argv[])
+{
+    Child ch1,ch2;
+    ch1.set_name((string)"Дмитрий");
+    ch1.set_family((string)"Ватулин");
+    ch1.set_age(18);
+    ch2.set_name((string)"Василий");
+    ch2.set_family((string)"Иванов");
+    ch2.set_age(18);
+    ch1.print_value();
+    ch2.print_value();
+    return 0;
+}
