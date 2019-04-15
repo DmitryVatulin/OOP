32d47e294168f9fe0ca278e29bb20144480c74ed
DmitryVatulin <47992419+DmitryVatulin@users.noreply.github.com>
2019-04-16 00:41:39 +0300

    Create pr8.1.cpp

---
+++ pr8/pr8.1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main() {
+    ofstream creating_file;
+    ifstream reading_file;
+    reading_file.open("file.txt");
+    if (reading_file.is_open()) {
+        string a;
+        while(!reading_file.eof()) {
+            reading_file >> a;
+            cout << a << endl;
+        }
+        reading_file.close();
+    } else {
+        cout << "File not found. Please write the data to a file.." << endl;
+        creating_file.open("file.txt",ios_base::trunc);
+        creating_file.close();
+    }
+    return 0;
+}
