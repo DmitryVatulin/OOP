29330df871ac628813c82c1c1edb896394410bfc
DmitryVatulin <47992419+DmitryVatulin@users.noreply.github.com>
2019-05-14 14:24:27 +0300

    Update lab5.1.cpp

--- lab5/lab5.1.cpp
+++ lab5/lab5.1.cpp
@@ -1,68 +1,82 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-class complex {
+class Complex {
+	double re;
+	double im;
 public:
-	double real;
-	double imag;
-	void operator +(complex);
-	void operator -(complex);
-	void operator =(complex);
-	void operator<<(complex);
-	void operator>>(complex);
-	bool friend operator==(complex a, complex b) {
-		if (a.real == b.real&&a.imag == b.imag)return true;
-		else return false;
-	}
-	
-	void output() {
-		cout << real << " " << imag << endl;
-	}
+	Complex() {}
+
+	Complex(double re, double im) : re(re), im(im) {}
+
+	Complex operator= (const Complex& c);
+
+	friend const Complex operator++ (Complex& c, int);
+
+	friend const Complex operator-- (Complex& c, int);
+
+	friend const bool operator== (const Complex& c1, const Complex& c2);
+
+	friend const ostream& operator<< (ostream& out, const Complex& c);
+
+	friend const istream& operator>> (istream& in, Complex& c);
+
+	~Complex() {}
 };
-void complex::operator +(complex a) {
-	real =real+a.real;
-	imag =imag+a.imag;
-}
-void complex::operator-(complex a) {
-	real = real - a.real;
-	imag = imag - a.imag;
-}
-void complex::operator=(complex a) {
-	real = a.real ;
-	imag = a.imag ;
-}
-void complex::operator<<(complex a) {
-	cout << a.real << " " << a.imag << endl;
-}
-void complex::operator>>(complex a) {
-	cin >> a.real;
-	cin>>a.imag;
-	this->real = a.real;
-	this->imag = a.imag;
+
+int main() {
+	Complex c1;
+	cout << "Enter complex number in format x+yi: ";
+	cin >> c1;
+
+
+	Complex c2 = c1;
+
+	if (c1 == c2) {
+		c2++;
+	}
+
+	c1--;
+
+	cout << c1;
+	cout << endl << c2;
+	cout << endl;
+
+	system("pause");
+	return 0;
 }
 
-int main()
-{
-	complex a = { 5,10 };
-	complex number = { -5,10 };
-	complex something;
-	complex number2 = {0,0};
-	int c;
-	cin >> c;
-	switch (c) {
-	case 1:
-		if (number == a) {
-			cout << "a==number" << endl;
-		}
-		else cout << "a!=number" << endl;
-		number2 >> number2;
-		number2 << number2;
-		number.output();
-		something = number;
-		something + a;
-		something.output();
-		number - a;
-		number.output();
-		break;
+Complex Complex::operator= (const Complex& c) { return Complex(c.re, c.im); }
+
+const Complex operator++ (Complex& c, int) {
+	Complex oldC(c);
+	c.re++;
+	return oldC;
+}
+
+const Complex operator-- (Complex& c, int) {
+	Complex oldC(c);
+	c.re--;
+	return oldC;
+}
+
+const bool operator== (const Complex& c1, const Complex& c2) {
+	return (c1.re == c2.re) && (c1.im == c2.im);
+}
+
+const ostream& operator<< (ostream& out, const Complex& c) {
+	out << c.re;
+	if (c.im < 0) {
+		out << c.im << "i";
+	} else {
+		out << "+" << c.im << "i";
 	}
+	return out;
+}
+
+const istream& operator>> (istream& in, Complex& c) {
+	in >> c.re >> c.im;
+	in.get();
+	return in;
 }
