91978b8c5943852bbc9c977feb130f63bfbac50f
DmitryVatulin <47992419+DmitryVatulin@users.noreply.github.com>
2019-05-14 14:26:22 +0300

    Update lab5.2.cpp

--- lab5/lab5.2.cpp
+++ lab5/lab5.2.cpp
@@ -3,48 +3,72 @@
 using namespace std;
 
 class Vector {
-    private:
-        float x;
-        float y;
-    public:
-        Vector(float a, float b) {
-            x = a;
-            y = b;
-        }
+	double x;
+	double y;
+	double z;
+public:
+	Vector() : x(0), y(0), z(0) {}
 
-        Vector& operator= (Vector& value) {
-            this->x = value.x;
-            this->y = value.y;
-            return *this;
-        };
+	Vector(double x, double y, double z) : x(x), y(y), z(z) {};
 
-        friend bool operator== (const Vector& left, const Vector& right) {
-            bool isX = left.x == right.x;
-            bool isY = left.y == right.y;
-            return isX && isY;
-        };
+	Vector(const Vector& v);
 
-        friend ostream& operator<< (ostream& out, const Vector& value) {
-            out << "( " << value.x << " , " << value.y << " )" << endl;
-            return out;
-        };
+	Vector operator= (const Vector& v);
 
-        friend istream& operator>> (istream& is, const Vector& value) {
-            is >> value.x >> value.y;
-            return is;
-        }
+	friend Vector operator+ (const Vector& v1, const Vector& v2);
 
-        float get_abs() {
-            return sqrt(x*x + y*y);
-        };
+	friend Vector operator- (const Vector& v1, const Vector& v2);
 
-        void plus(Vector b) {
-            x += b.x;
-            y += b.y;
-        };
-        
-        void minus(Vector b) {
-            x -= b.x;
-            y -= b.y;
-        };
+	friend const bool operator== (const Vector& v1, const Vector& v2);
+
+	friend const istream& operator>> (istream& in, Vector& v);
+
+	friend const ostream& operator<< (ostream& out, const Vector& v);
 };
+
+int main() {
+	cout << "Set coordinates of the vector:";
+	Vector v1;
+	cin >> v1;
+	Vector v2 = v1;
+
+	if (v1 == v2) {
+		cout << v1 + v2;
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
+
+Vector::Vector(const Vector& v) {
+	x = v.x;
+	y = v.y;
+	z = v.z;
+}
+
+Vector Vector::operator= (const Vector& v) {
+	return Vector(v.x, v.y, v.z);
+}
+
+Vector operator+ (const Vector& v1, const Vector& v2) {
+	return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
+}
+
+Vector operator- (const Vector& v1, const Vector& v2) {
+	return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
+const bool operator== (const Vector& v1, const Vector& v2) {
+	return (v1.x == v2.x) && (v1.y == v2.y) && (v1.z == v2.z);
+}
+
+const istream& operator>> (istream& in, Vector& v) {
+	in >> v.x >> v.y >> v.z;
+	return in;
+}
+
+const ostream& operator<< (ostream& out, const Vector& v) {
+	out << v.x << " " << v.y << " " << v.z;
+	return out;
+}
